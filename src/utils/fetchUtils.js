7480646339a891fac9a89b6ab5bd905d38585eb0
Simran Johal <134616481+simran-johal@users.noreply.github.com>
2024-06-03 19:16:58 +0100

    Implement refresh token flow into fetchutils functions

--- src/utils/fetchUtils.js
+++ src/utils/fetchUtils.js
@@ -1,14 +1,23 @@
+import { refreshAccessToken } from "./authUtils";
 
-
-
-export const fetchDataWithToken = async (accessToken, searchTerm) => {
+const constructUrl = (searchTerm) => {
     const baseUrl = 'https://api.spotify.com/v1' 
     const endpoint = '/search' 
     const limit = 20;
     const type = 'track, artist, album'
 
     const requestParams = `?q=${encodeURIComponent(searchTerm)}&type=${type}&limit=${limit}`;
-    const urlToFetch = `${baseUrl}${endpoint}${requestParams}`
+    return `${baseUrl}${endpoint}${requestParams}`
+}
+
+
+export const fetchDataWithToken = async (searchTerm) => {
+    const urlToFetch = constructUrl(searchTerm);
+
+    let accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+        throw new Error('No access token available')
+    }
 
     try {
 
@@ -18,15 +27,33 @@ export const fetchDataWithToken = async (accessToken, searchTerm) => {
             }
         });
 
-        if (!response.ok) {
+        // refresh token code starts
+        if (response.status === 401) {
+            try {
+                accessToken = await refreshAccessToken(); // if response erro refresh access token
+                if (!accessToken) {                     // if fails then throw error telling us
+                    throw new Error('Failed to refresh access token');
+                }
+
+                response = await fetch(urlToFetch, {    // if get new accessToken try to fetch now
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`
+                    }
+                })
+            } catch (refreshError) {                    // if fails tell us
+                throw new Error('Error refreshing access token: ' + refreshError.message)
+            }
+        }
+
+        // refresh token code ends
+
+        if (!response.ok) {                             // if neither 
             throw new Error('Network response was not ok');
         };
 
-        const data = await response.json();
-        return data; // maybe not
+        return await response.json();                   // once successful refresh/access + got response
+    } catch (fetchError) {
+        throw new Error('Error fetching data: ' + fetchError.message)
 
-    } catch (error) {
-        console.log('Fetch error:', error);
-        throw error;
     }
 }
